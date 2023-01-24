cc5f4dfba361eed09086ab4202a62ed3ecab07a5
himani <himanijkothari@gmail.com>
2023-01-24 23:10:00 +0100

    temperature screen with hard coded values

--- app/src/main/java/com/example/weatherapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/weatherapp/ui/theme/Color.kt
@@ -1,8 +1,11 @@
 package com.example.weatherapp.ui.theme
 
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+val Darktheme = Color(0xFF121212)
+val Blue100 = Color(0xFF063a5b)
