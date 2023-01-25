79c77f113ae744ffaea47662d469cf6937a46701
himani <himanijkothari@gmail.com>
2023-01-25 22:19:54 +0100

    Adding bottom app bar, basic forecast and menu screen

--- app/src/main/java/com/example/weatherapp/WeatherAppContent.kt
+++ app/src/main/java/com/example/weatherapp/WeatherAppContent.kt
@@ -1,20 +1,104 @@
 package com.example.weatherapp
 
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import com.example.weatherapp.ui.screens.HomeScreen
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.weatherapp.ui.screens.WeatherScreen
+import com.example.weatherapp.ui.theme.Darktheme
+
 
 @Composable
 fun WeatherAppContent() {
+
+    val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+    val bottomTabRoutes = listOf(Weather,Forecast, Menu)
     
     Scaffold(
-        topBar = {
-
-        }
+        bottomBar = { BottomAppBar(
+            modifier = Modifier
+                // .padding(vertical = 4.dp, horizontal = 24.dp)
+                .height(88.dp)
+                .clip(RoundedCornerShape(16.dp)),
+            elevation = 0.dp,
+            backgroundColor = Darktheme
+        ){
+            bottomTabRoutes.forEach { screen ->
+                NavButton(
+                    modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                    screens = screen,
+                    selected = screen.route == currentRoute
+                ) {
+                    navController.navigate(screen.route) {
+                        popUpTo(navController.graph.startDestinationId) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                    }
+                }
+            }
+        } }
     ) {
-        HomeScreen()
+        WeatherScreen()
     }
+}
+
+@Composable
+fun NavButton(
+    modifier: Modifier = Modifier,
+    screens: Screens,
+    selected : Boolean = false,
+    onClick: () -> Unit = {}
+) {
+    Box(
+        modifier = modifier
+            .padding(8.dp)
+            .size(82.dp)
+            .clickable(
+              indication = null,
+              interactionSource = remember { MutableInteractionSource() },
+              onClick = onClick)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(top = 8.dp, bottom = 4.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val color = if (selected) Color.Blue else Color.DarkGray
+            Icon(
+                Icons.Filled.LocationOn,
+                null,
+                tint = color,
+                modifier = Modifier.size(24.dp, 24.dp)
+            )
+            Text(
+                text = stringResource(id = screens.resourceId),
+                color = color,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.button.copy(fontSize = 12.sp, color = color)
+            )
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/weatherapp/ui/screens/ForecastScreen.kt
@@ -0,0 +1,12 @@
+package com.example.weatherapp.ui.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ForecastScreen() {
+
+    Text(
+        text = "forecast screen"
+    )
+}
